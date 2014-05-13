ec0eb719c67527e71a19bc1a2c5fe9d534830681
mikenewt <mikejnewt@gmail.com>
2014-05-13 09:03:26 +0800

    Done!

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 $(document).ready(function() { 
-	$("#enter-number").keyup(function() {
-			var value = $(this).val();
-			console.log(value);
-			doFizzBuzz(value);
+	$("#start").click(function(event) {
+
+		for (i = 1; i <= 100; i++) {
+			if ( i % 15 == 0) {
+				console.log("fizzbuzz");
+				$("body").append("<p>fizzbuzz</p>");
+			} else if ( i % 3 == 0 ) {
+				console.log("fizz");
+				$("body").append("<p>fizz</p>");
+			} else if ( i % 5 == 0) {
+				console.log("buzz");
+				$("body").append("<p>buzz</p>");
+			} else {
+				console.log(i);
+				$("body").append("<p>" + i + "</p>");
+			}
+		};
 	});
 });
 
-function doFizzBuzz(v) {
-	for (i = 0; i <= v; i++) {
+function doFizzBuzz(value) {
+	for (i = 0; i <= value; i++) {
 		if ( i % 3 == 0) {
 			i = "fizz";
+			console.log(i);
 		} else if ( i % 5 == 0 ) {
 			i = "buzz";
+			console.log(i);
 		} else if ( i % 3 == 0 && i % 5 == 0 ) {
 			i = "fizzbuzz";
+			console.log(i);
 		} else {
 			console.log(i);
 		};
