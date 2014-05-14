d9ad0bd0d197597fb958da8988d81bd55de6f455
mikenewt <mikejnewt@gmail.com>
2014-05-14 20:46:50 +0800

    Refactored with function.
    Added text input with function support.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 $(document).ready(function() { 
+	$("#input").keydown(function(event) {
+		if (event.which == 13) {
+			var number = Number($(this).val());
+			doFizzBuzz(number);
+		}
+	});
+
 	$("#start").click(function(event) {
 
 		for (i = 1; i <= 100; i++) {
@@ -18,3 +25,21 @@ $(document).ready(function() {
 		};
 	});
 });
+
+function doFizzBuzz(number) {
+	for (i = 1; i <= number; i++) {
+		if ( i % 15 == 0) {
+			console.log("fizzbuzz");
+			$("body").append("<p>fizzbuzz</p>");
+		} else if ( i % 3 == 0 ) {
+			console.log("fizz");
+			$("body").append("<p>fizz</p>");
+		} else if ( i % 5 == 0) {
+			console.log("buzz");
+			$("body").append("<p>buzz</p>");
+		} else {
+			console.log(i);
+			$("body").append("<p>" + i + "</p>");
+		}
+	};	
+}
